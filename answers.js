a16b75855e17a07a40dd0bc565c7c6eabc106c13
Dylan Pelletier <dylan.pelletier1979@gmail.com>
2016-07-07 19:53:30 +0000

    Finished up to Exercise 5

--- answers.js
+++ answers.js
@@ -1,6 +1,6 @@
 // Exercise 1
 
-var positives = [1,2,3,-4,6,9,12,1-15,17,-29,44];
+// var positives = [1,2,3,-4,6,9,12,1-15,17,-29,44];
 
 /* function printPositives(array){
     array.forEach(function(number){
@@ -25,14 +25,14 @@ console.log(filteredArray); */
 
 // Exercise 2B
 
-function findPositives (number) {
+/* function findPositives (number) {
      if(number > 0){
          return number;
      }
-};
+}; */
 
 /* function printPositives(array){
-     return positives.filter(findPositives).forEach(function(number){
+     return array.filter(findPositives).forEach(function(number){
          console.log(number);
         
      });
@@ -40,4 +40,44 @@ function findPositives (number) {
 
 printPositives(positives); */
 
-// // Exercise 3
+// Exercise 3
+
+/* var truthy = ['bob', 7, 0, false]
+
+function findThePositives (value) {
+    if (value){
+        return value;
+    }
+}
+function filterArray (function1,array1) {
+     return array1.filter(function1);
+}
+
+console.log(filterArray(findThePositives,truthy));
+
+// Exercise 4
+var string1 = "Dylan is learning to write functions in Javascript";
+var splitArray = string1.split(" ");
+
+var a = splitArray.reduce(function longestWord(initWord, word){
+    if (word.length > initWord.length ){
+        initWord = word
+    } 
+    return initWord;
+}, "");
+
+console.log(a); */
+
+// Exercise 5
+var string1 = "Dylan is learning to write functions in Javascript";
+var splitCharacterArray = string1.split("");
+
+var b = splitCharacterArray.reduce(function countVowels(counter, newChar){
+    var vowels = ['a','e','i','o','u'];
+    if (vowels.indexOf(newChar) > -1){
+       counter = counter + 1;
+    } 
+    return counter;
+},0);
+
+console.log(b);
