47c8ee4d0f13ef12206c73c50a4b3cee33cd0980
Dylan Pelletier <dylan.pelletier1979@gmail.com>
2016-07-07 18:00:03 +0000

    Finished Exercises 2A and 2B

--- answers.js
+++ answers.js
@@ -1,3 +1,5 @@
+// Exercise 1
+
 var positives = [1,2,3,5,6,9,12,15,17,29,44];
 
 function printPositives(number){
@@ -7,5 +9,37 @@ function printPositives(number){
         }
     });        
 }
+
+//printPositives();
+
+// Exercise 2
+
+var positives = [1,2,3,5,6,9,12,15,17,29,44];
+
+function findPositives (number) {
+    if(number % 2 === 0){
+        return number;
+    }
+};
+
+var filteredArray = positives.filter(findPositives);
+// console.log(filteredArray);
+
+// Exercise 3
+
+var positives = [1,2,3,5,6,9,12,15,17,29,44];
+
+function findPositives (number) {
+    if(number % 2 === 0){
+        return number;
+    }
+};
+
+function printPositives(number){
+    return positives.filter(findPositives).forEach(function(number){
+        console.log(number);
+        
+    });
+};
 
 printPositives();
